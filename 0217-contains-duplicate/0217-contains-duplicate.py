908bca06dc70876fef75ecad1ce2094b4984fa9c
Mohamed Sahnoun <54535672+MedNoun@users.noreply.github.com>
2023-10-15 14:43:18 +0100

    Time: 1674 ms (5.1%), Space: 28.3 MB (93.67%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,12 +1,43 @@
 class Solution:
+    def mergeSort(self,arr):
+        if(len(arr)>1):
+            mid = len(arr)//2
+            left = arr[:mid]
+            right = arr[mid:]
+            s_left = self.mergeSort(left)
+            s_right = self.mergeSort(right)
+            i =0
+            j =0
+            counter = 0
+            while(i<len(s_left) and j<len(s_right)):
+                if(s_left[i]<s_right[j]):
+                    arr[counter] = s_left[i]
+                    i+=1
+                else:
+                    arr[counter] = s_right[j]
+                    j+=1
+                counter+=1
+
+            while(i<len(s_left)):
+                arr[counter] = s_left[i]
+                i+=1
+                counter+=1
+
+            while(j<len(s_right)):
+                arr[counter] = s_right[j]
+                j+=1
+                counter+=1
+                
+        return arr
+    
     
     def containsDuplicate(self, nums: List[int]) -> bool:
-        dic = dict()
-        for el in nums:
-            dic[el] = 0
-        for el in nums:
-            dic[el] += 1
-            if(dic[el] == 2): return True
+        length = len(nums)
+        sort = self.mergeSort(nums)
+        for i in range(length - 1) :
+            if(nums[i] == nums[i+1]):
+                return True
+            
         return False
             
         
