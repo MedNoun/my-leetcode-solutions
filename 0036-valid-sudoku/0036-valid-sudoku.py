97e4fc53c24d0c4174471d30f1f0ae7d0bd28d24
Mohamed Sahnoun <54535672+MedNoun@users.noreply.github.com>
2023-10-15 15:48:34 +0100

    Time: 101 ms (63.14%), Space: 16.4 MB (31.95%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,9 +1,31 @@
-class Solution(object):
-    def isValidSudoku(self, board):
-        res = []
-        for i in range(9):
-            for j in range(9):
-                element = board[i][j]
-                if element != '.':
-                    res += [(i, element), (element, j), (i // 3, j // 3, element)]
-        return len(res) == len(set(res))
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        for i in range(3):
+            for j in range(3):
+                occ = {}
+                for k in range(i*3,i*3+3):
+                    for m in range(j*3,j*3+3):
+                        num = board[k][m]
+                        element = occ.get(num)
+                        if(element and num != "."):
+                            return False
+                        else:
+                            occ[num] = 1  
+        for line in board:
+            occ = {}
+            for el in line:
+                element = occ.get(el)
+                if(element and el != "."):
+                    return False
+                else:
+                    occ[el] = 1
+        for col in zip(*board):
+            occ = {}
+            for el in col:
+                element = occ.get(el)
+                if(element and el != "."):
+                    return False
+                else:
+                    occ[el] = 1 
+        
+        return True  
