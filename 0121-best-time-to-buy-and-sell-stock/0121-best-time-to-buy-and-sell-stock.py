8b046fe846af268ed3cdb4a1598bf5d25eb09591
Mohamed Sahnoun <54535672+MedNoun@users.noreply.github.com>
2023-10-22 20:29:16 +0100

    Time: 866 ms (53.77%), Space: 27.1 MB (93.75%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        l, r = 0,1
+        profit = 0
+        while(r<len(prices)):
+            if(prices[r] > prices[l]):
+                profit = max(profit, prices[r] - prices[l])
+            else:
+                l = r
+            r+=1
+        return profit
+        
