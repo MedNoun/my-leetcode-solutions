e936f758a8933563f0b5b12eabeb92dde6b0bdb9
Mohamed Sahnoun <54535672+MedNoun@users.noreply.github.com>
2026-02-26 10:49:55 +0100

    Time: 7 ms (90.57%), Space: 19.5 MB (64.29%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,13 +1,14 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        mem = {}
-        for c in s:
-            mem[c] = mem.get(c, 0) + 1
-        for c in t:
-            if not mem.get(c, None):
+        res = [0] * 26
+        if len(s) != len(t):
+            return False
+        for i in range(len(s)):
+            res[ord(s[i]) - 97] += 1
+            res[ord(t[i]) - 97] -= 1
+        for v in res:
+            if v != 0 :
                 return False
-            mem[c]-=1
-        occ = set(mem.values())
-        return len(occ)==1 and 0 in occ
+        return True
 
         
