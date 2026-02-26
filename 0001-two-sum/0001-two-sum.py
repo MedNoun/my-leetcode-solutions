12126067475ee309d0f44de62b6e60c1661e7d95
Mohamed Sahnoun <54535672+MedNoun@users.noreply.github.com>
2026-02-26 11:28:05 +0100

    Time: 0 ms (100%), Space: 20.4 MB (51.55%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dic_nums = [{"i" : k, "v" : nums[k]} for k in range(len(nums))]
-        nums = sorted(dic_nums, key=lambda x : x.get("v"))
-        i , j = 0 , len(nums) - 1
-        while i<j:
-            s = nums[i]["v"] + nums[j]["v"]
-            if s == target:
-                return [nums[i]["i"],nums[j]["i"]]
-            if s < target:
-                i+=1
-            if s > target:
-                j-=1
-        return []
+        t_val = {}
+        for i in range(len(nums)):
+            j = t_val.get(target - nums[i], None)
+            if j != None :
+                return [i, j]
+            t_val[nums[i]] = i
