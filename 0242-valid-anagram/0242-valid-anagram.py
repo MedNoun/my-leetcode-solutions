49b42f402e3267ba383d849ec9951caadd97ec6c
Mohamed Sahnoun <54535672+MedNoun@users.noreply.github.com>
2026-02-26 10:40:18 +0100

    Time: 740 ms (5.87%), Space: 19.6 MB (38.12%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,16 +1,12 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        chars = {}
-        for char in t:
-            try:
-                chars[char] +=1
-            except:
-                chars[char] = 1
-        for char in s:
-            try:
-                chars[char] -=1
-            except:
-                return False
-        for el in chars:
-            if chars[el]!=0: return False
-        return True
+        primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89,  97, 101]
+        s_hash = 1
+        t_hash = 1
+        for i in range(min(len(s), len(t))):
+            s_hash *= primes[ord(s[i]) - 97]
+            t_hash *= primes[ord(t[i]) - 97]
+
+        return s_hash == t_hash and len(s) == len(t)
+
+        
