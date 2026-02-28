a1b22be28662d8592cd0df6408ab462fed606ea3
Mohamed Sahnoun <54535672+MedNoun@users.noreply.github.com>
2026-02-28 15:03:46 +0100

    Time: 11 ms (24.96%), Space: 24.7 MB (23.28%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,14 +1,17 @@
-import heapq
 class Solution:
-    def topKFrequent(self, nums, k):
-        out = {}
-        for num in nums:
-            try:
-                out[num]+=1
-            except:
-                out[num]= 1
-        pq = []
-        for el in out:
-            heapq.heappush(pq,(out[el],el))
-            if len(pq)>k: heappop(pq)
-        return [i[1] for i in pq]
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        occ = {}
+        for n in nums:
+            occ[n] = occ.get(n,0) + 1
+        b_sort = [[] for _ in range(len(nums) + 1)]
+        for ky,v in occ.items():
+            b_sort[v].append(ky)
+        i = len(nums)
+        res = []
+        while k>0:
+            while len(b_sort[i]) == 0:
+                i -=1
+            res.append(b_sort[i].pop())
+            k-=1
+        return res
+        
